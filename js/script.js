bec5d1a893004ea95f7865cd5b6e50314f4260ea
Krzysztof Mirończuk <krzysztof.mironczuk@socart.media>
2022-04-11 13:11:52 +0200

    Finished generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -44,9 +44,47 @@ const titleClickHandler = function(event){
 
   }
   
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  for(let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+    
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    /* insert link into html variable */
+    html = html + linkHTML;
+  }
+  
+  titleList.innerHTML = html;
+
   const links = document.querySelectorAll('.titles a');
+  console.log(links);
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
-  
+
+}
+
+
+generateTitleLinks();
